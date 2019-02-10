eaf95b0ece810b71c450d2f04d0aac36ad1de367
Anatolijes <anatolijes@gmail.com>
2019-02-10 15:56:09 +0000

    Add couple feature!!! from now on you can pass to the bot, /start, "хуй", and "покажи сиськи" which will, give you immediate response! Cool isn it?!

---
+++ main/java/storage/UserStorage.java
@@ -0,0 +1,12 @@
+package storage;
+
+import model.BotUser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserStorage {
+
+	Map<Integer, BotUser> user = new HashMap<Integer, BotUser>();
+
+}
