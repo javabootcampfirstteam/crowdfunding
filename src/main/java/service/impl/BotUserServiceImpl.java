c8457949700e15ae06ff0e9eafd499549c504db0
Prokhor <DbIM@mail.ru>
2019-02-11 17:04:55 +0300

    commmit 3

---
+++ src/main/java/service/impl/BotUserServiceImpl.java
@@ -0,0 +1,42 @@
+package service.impl;
+
+import dao.abstr.BotUserDao;
+import dao.impl.BotUserDaoImpl;
+import model.BotUser;
+import service.abstr.BotUserService;
+
+public class BotUserServiceImpl implements BotUserService {
+
+
+	private BotUserServiceImpl() {}
+	private static BotUserServiceImpl instance;
+	public static BotUserServiceImpl getInstance(){
+		if (instance == null) {
+			instance = new BotUserServiceImpl();
+		}
+		return instance;
+	}
+
+	private BotUserDao botUserDao = BotUserDaoImpl.getInstance();
+
+
+	@Override
+	public void addUser(BotUser botUser, Integer id) {
+	  	botUserDao.addUser(botUser, id);
+	}
+
+	@Override
+	public BotUser getUser(Integer id) {
+		return botUserDao.getBotUserById(id);
+	}
+
+
+	@Override
+	public boolean isUserExistById(Integer id) {
+		if(botUserDao.getBotUserById(id)!= null ){
+			return true;
+		}
+		return false;
+	}
+
+}
