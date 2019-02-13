ab8f82f5da9745963f88a5be2db2962a25591a41
Anatolijes <anatolijes@gmail.com>
2019-02-13 15:28:13 +0000

    Last common project code

--- src/main/java/UrbanSocializerBot.java
+++ src/main/java/UrbanSocializerBot.java
@@ -50,7 +50,7 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 
 	public void onUpdateReceived(Update update) {
 
-		List<String> currentContext = new ArrayList<>();
+		List<String> currentContext;
 		Message message = update.getMessage();
 
 		if (message != null & message.hasText()) {
@@ -59,64 +59,38 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 			String telegramUserName = userFromTelegram.getFirstName();
 			Integer currentUserId = userFromTelegram.getId();
 			Long currentChatId = message.getChatId();
-			//здесть не прававельно создается екземпляр BotUser
+			int contextPosition = 0;
 			BotUser botUser = new BotUser();
+			//здесть не прававельно создается екземпляр BotUser
+
 			if ("/start".equals(messageFromTelegram)) {
-				botUserService.addUser(botUser, currentUserId);
-				sendMsg(currentChatId, "Привет " + telegramUserName + ", вы впервые у нас, добавляем вас в базу");
-				sendMsg(currentChatId, "/reg - регистрация\n/info - информация");
-
-			} else {
-
-
+				if (!botUserService.isUserExistById(currentUserId)) {
+					botUserService.addUser(new BotUser(), currentUserId);
+					sendMsg(currentChatId, "Добрый день, вы впервые у нас, добавляем вас в базу\n");
+					sendMsg(currentChatId, "/reg - регистрация\n/info - информация");
+				} else {
+					sendMsg(currentChatId, "Привет " + telegramUserName + ", Мы уже знакомы с вами!");
+					sendMsg(currentChatId, "/info - информация");
+				}
+			}
+			else {
 				BotUser currentUser = botUserService.getUser(currentUserId);
 				currentContext = currentUser.getContext();
 
-
 				if (currentContext.isEmpty()) {
-
 					switch (messageFromTelegram) {
 						case "/reg": {
-							sendMsg(currentChatId, "регстрация");
-							sendMsg(currentChatId, "Введитте имя");
-							setContextToUser(currentUserId, "/reg");
+							sendMsg(currentChatId, "Пожалуйста укажите город где вы живете.");
+							currentContext.add("/regCity");
 							break;
 						}
 						case "/info": {
-							sendMsg(currentChatId, "информация");
-							setContextToUser(currentUserId, "/info");
-							break;
+							sendMsg(currentChatId, "Добро пожаловать.\n" +
+									"Здесь вы можете предложить свой проект " +
+									"по улучшению городской или районной инфраструктуры. /add_project\n"
+									+ "или ознакомится с текущими предложениями. /list_project");
 						}
-						default: {
-							sendMsg(currentChatId, "неизвестная команда");
-						}
-					}
-
-
-				} else {
-					int contextPosition = 0;
-					switch (currentContext.get(contextPosition++)) {
-						case "/reg": {
-
-							if (currentContext.size() == contextPosition) {
-								//ДОБАВЛЕНИЕ  ИМЕНИ
-							} else {
-								switch (currentContext.get(contextPosition++)) {
-								}
-							}
-							break;
-						}
-						case "/info": {
-
-
-							break;
-						}
-						case "/list_activities": {
-							// проверка роли
-							sendMsg(currentChatId, "список");
-							break;
-						}
-						case "show_active_projects": {
+						case "/show_active_projects": {
 							ProjectDaoImpl projectDao = new ProjectDaoImpl();
 
 							//projectDao.getProjectById()
@@ -124,24 +98,24 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 						}
 						case "add_projects": {
 							ProjectDaoImpl projectDao = new ProjectDaoImpl();
-                            Project project = new Project();
+							Project project = new Project();
 
 							LocalDateTime localDateTime = LocalDateTime.now();
 							project.setProjectName(messageFromTelegram);
-                            project.setProjectDateCreate(localDateTime);
-                            project.setAuthorName("Petr");
-                            project.setProjectDescription(messageFromTelegram);
-                            project.setProjectId(project.getProjectId());
-                            project.setAuthorSurname("Hardcock");
-                            project.setProjectSum(2323);
-                            PROJECT_ID++;
+							project.setProjectDateCreate(localDateTime);
+							project.setAuthorName("Petr");
+							project.setProjectDescription(messageFromTelegram);
+							project.setProjectId(project.getProjectId());
+							project.setAuthorSurname("Hardcock");
+							project.setProjectSum(2323);
+							PROJECT_ID++;
 							projectDao.addProject(project, PROJECT_ID);
 
 							break;
 						}
 						case "test_kf": {
 							KeyboardFactory keyboardFactory = new KeyboardFactory();
-							keyboardFactory.createKeyboardRow(2,3,true,true,true, "test1", "test2", "test3");
+							keyboardFactory.createKeyboardRow(2, 3, true, true, true, "test1", "test2", "test3");
 
 							//projectDao.getProjectById()
 							break;
@@ -150,12 +124,49 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 						default: {
 							sendMsg(currentChatId, "неизвестная команда");
 						}
+
+					} else  {
+						switch (currentContext.get(contextPosition)) {
+							case "/regCity": {
+								if (currentContext.size() == ++contextPosition) {
+									String cityReg = message.getText();
+									currentUser.setUserAddress(cityReg);
+									sendMsg(currentChatId, "Пожалуйста укажите свой телефон.");
+									currentContext.add("/regPhone");
+								} else {
+									switch (currentContext.get(contextPosition)) {
+										case "/regPhone": {
+											if (currentContext.size() == ++contextPosition) {
+												String phoneReg = message.getText();
+												currentUser.setPhoneNumber(Integer.parseInt(phoneReg));
+												sendMsg(currentChatId, "Добро пожаловать: " + telegramUserName + "\n"
+														+ "из города " + currentUser.getUserAddress() + ", тел. номер: "
+														+ currentUser.getPhoneNumber() + "\n");
+												currentContext.clear();
+												sendMsg(currentChatId, "Благодарим за регистрацию. " +
+														"Теперь посмотрите что люди предлагают.\n /info");
+											}
+											break;
+										}
+									}
+								}
+								break;
+							}
+
+							case "/list_project": {
+								sendMsg(currentChatId, "Здесь будут перечислены текущие проекты.");
+								break;
+							}
+
+							default: {
+								sendMsg(currentChatId, "неизвестная команда");
+							}
+						}
 					}
 				}
 			}
 		}
 	}
-
 
 
 	// просто тестовый метод
@@ -211,7 +222,8 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 			e.printStackTrace();
 		}
 	}
-	//private void getProjectList(int indexFrom, int indexTo){ }
+
+
 	public String getBotUsername() {
 		return BOT_NAME;
 	}
@@ -219,181 +231,3 @@ public class UrbanSocializerBot extends TelegramLongPollingBot implements Serial
 
 
 
-/*
-*
-* public void onUpdateReceived(Update update) {
-		// We check if the update has a message and the message has text
-		//достаем userId и userName из полученного сообщения
-		String userName = update.getMessage().getFrom().getFirstName();
-		Integer userId = update.getMessage().getFrom().getId();
-		String message_text = update.getMessage().getText();
-		long chat_id = update.getMessage().getChatId();
-		SendMessage message = new SendMessage() // Create a message object object
-				.setChatId(chat_id)
-				.setText(message_text);
-
-
-		switch (message_text) {
-			case "/start":
-				if (BotUserServiceImpl.getInstance().isUserExistById(userId)) {
-					message.setText(userName + userId);
-				}
-				try {
-					execute(message);  // Sending our message object to USERS_TABLE
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-
-				break;
-
-			case "/login":
-				//пробуюю проверить зареган ли клиен
-				// и если нет то внести в базу
-				if (BotUserServiceImpl.getInstance().isUserExistById(userId)) {
-					message.setText(userName + userId);
-				} else {
-					//create user, set user fields, and add to the storage.
-					BotUser botUser = new BotUser();
-					botUser.setUserId(userId);
-					botUser.setUserName(userName);
-					botUser.setUserAddress("user address");
-					BotUserServiceImpl.getInstance().addUser(botUser, userId);
-					USERS_TABLE.put(botUser.getUserId(), botUser);
-				}
-
-				try {
-					execute(message);  // Sending our message object to USERS_TABLE
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-
-				break;
-
-			case "/createProject":
-
-				List<KeyboardRow> keyboard1 = new ArrayList<>();
-				KeyboardRow keyboardRowOne = new KeyboardRow();
-				ReplyKeyboard replyKeyboard1 = new ReplyKeyboardRemove();
-				keyboardRowOne.add("repKeyRemove");
-				keyboard1.add(keyboardRowOne);
-				//ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-
-				message = new SendMessage().setChatId(chat_id).setText("test");
-				try {
-					execute(message);
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-
-				break;
-			case "/help":
-				// выводит список доступных команд для для пользователя
-				message = new SendMessage().setChatId(chat_id).setText(
-						"See list of the command\n" +
-								"/help\n" +
-								"/projectList\n" +
-								"/pay\n" +
-								"/menu\n" +
-								"/beck" +
-								"/createProject");
-
-
-				try {
-					execute(message);
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-				break;
-			case "/test":
-				// пока что сдесть просто пробуюю как работают кнопки
-				Message message1 = new Message();
-				SendMessage sendMessage1 = new SendMessage();
-				sendMessage1.enableMarkdown(true);
-
-				// Создаем клавиуатуру
-				ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-				sendMessage1.setReplyMarkup(replyKeyboardMarkup);
-
-				// Опционально. Этот параметр нужен, чтобы показывать
-				// клавиатуру только определённым пользователям.
-				// Цели: 1) пользователи, которые были @упомянуты
-				// в поле text объекта Message;
-				// 2) если сообщения бота является ответом
-				// (содержит поле reply_to_message_id), авторы этого сообщения.
-				replyKeyboardMarkup.setSelective(true);
-
-				// Опционально. Указывает клиенту подогнать
-				// высоту клавиатуры под количество кнопок
-				// (сделать её меньше, если кнопок мало).
-				// По умолчанию False, то есть клавиатура
-				// всегда такого же размера, как и стандартная клавиатура устройства.
-				replyKeyboardMarkup.setResizeKeyboard(true);
-
-
-				// iОпционально. Указывает клиенту скрыть клавиатуру после
-				// использования (после нажатия на кнопку). Её по-прежнему
-				// можно будет открыть через иконку в поле ввода сообщения. По умолчанию False.
-				replyKeyboardMarkup.setOneTimeKeyboard(true);
-
-				// Создаем список строк клавиатуры
-				List<KeyboardRow> keyboard = new ArrayList<>();
-
-				// Первая строчка клавиатуры
-				KeyboardRow keyboardFirstRow = new KeyboardRow();
-				// Добавляем кнопки в первую строчку клавиатуры
-				keyboardFirstRow.add("project");
-				keyboardFirstRow.add("help");
-				keyboardFirstRow.add("help2");
-
-
-				// Вторая строчка клавиатуры
-				KeyboardRow keyboardSecondRow = new KeyboardRow();
-				// Добавляем кнопки во вторую строчку клавиатуры
-				keyboardSecondRow.add("donate");
-				keyboardSecondRow.add("contact");
-				keyboardSecondRow.add("contact2");
-
-				//Third row keyboard
-				KeyboardRow keyboardThirdRow = new KeyboardRow();
-
-				// Add buttons into the third row of the keyboard
-
-				keyboardThirdRow.add("exit");
-				keyboardThirdRow.add("beck");
-
-				// Добавляем все строчки клавиатуры в список
-				keyboard.add(keyboardFirstRow);
-				keyboard.add(keyboardSecondRow);
-				keyboard.add(keyboardThirdRow);
-				// устанваливаем этот список нашей клавиатуре
-				replyKeyboardMarkup.setKeyboard(keyboard);
-
-				sendMessage1.setChatId(message.getChatId().toString());
-				sendMessage1.setReplyToMessageId(message1.getMessageId());
-				sendMessage1.setText("project");
-				try {
-					execute(sendMessage1);
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-
-				break;
-			case "/project1":
-
-				//	List<KeyboardButton> keyboardButtons = new ArrayList<>();
-				//	KeyboardButton keyboardButton = new KeyboardButton();
-				//	KeyboardRow keyboardRow = new KeyboardRow();
-				//	ReplyKeyboard replyKeyboard = new ReplyKeyboardMarkup();
-
-			case "/exit":
-				//ReplyKeyboardMarkup replyKeyboardMarkup1 = createYesOrNoKeyboard();
-
-				try {
-
-					execute(message);
-				} catch (TelegramApiException e) {
-					e.printStackTrace();
-				}
-				break;
-		}
-	}*/
